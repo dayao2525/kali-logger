5021115c0548ffe58c1262ad1b0842eba3638af1
dayao <dayao2525@gmail.com>
2023-05-30 17:49:46 +0700

    更新声明文件

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,25 @@
-import { Telegraf } from "telegraf";
-import type winston from "winston";
-
-export interface LoggerInstance {
-  error: winston.LeveledLogMethod;
-  log: winston.LeveledLogMethod;
+declare module '@dayao2525/logger' {
+  import { Telegraf } from "telegraf";
+  import type winston from "winston";
+  export interface LoggerInstance {
+    error: winston.LeveledLogMethod;
+    log: winston.LeveledLogMethod;
+  }
+  
+  export interface LoggerOptions {
+    dir: string;
+    debug: boolean;
+  }
+  
+  interface Constructor {
+    (options: LoggerOptions): LoggerInstance;
+  }
+  
+  export interface BotInstance {
+    bot:  Telegraf,
+    text: (chatId: string, msg: string)=> void
+  }
+  
+  export const Logger: Constructor;
+  export const Bot: (key: string)=> BotInstance
 }
-
-export interface LoggerOptions {
-  dir: string;
-  debug: boolean;
-}
-
-interface Constructor {
-  (options: LoggerOptions): LoggerInstance;
-}
-
-export interface BotInstance {
-  bot:  Telegraf,
-  text: (chatId: string, msg: string)=> void
-}
-
-export const Logger: Constructor;
-export const Bot: (key: string)=> BotInstance
